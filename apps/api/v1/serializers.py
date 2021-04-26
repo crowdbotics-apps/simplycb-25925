993629ede09c0ae90c450022532775da756e8d33
Muhammad Abdullah Abid <hi@mabdullahabid.com>
2021-04-26 00:17:29 -0700

    overriding create for serializer

--- apps/api/v1/serializers.py
+++ apps/api/v1/serializers.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest
+
 from rest_framework import serializers
 
 from apps.models import App
@@ -8,3 +10,24 @@ class AppSerializer(serializers.ModelSerializer):
         model = App
         fields = "__all__"
         read_only_fields = ["user"]
+
+    def _get_request(self):
+        request = self.context.get("request")
+        if (
+            request
+            and not isinstance(request, HttpRequest)
+            and hasattr(request, "_request")
+        ):
+            request = request._request
+        return request
+
+    def create(self, validated_data):
+        """
+        Override the create function here to associate the created App
+        with the User who created it
+        """
+        app = App.objects.create(**validated_data)
+        request = self._get_request()
+        app.user = request.user
+        app.save()
+        return app

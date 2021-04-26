5fbefe56f2d0697cd23aab6f2c7ac21c2bd4fd08
Muhammad Abdullah Abid <hi@mabdullahabid.com>
2021-04-26 00:18:00 -0700

    initial migrations and filter queryset

--- apps/api/v1/viewsets.py
+++ apps/api/v1/viewsets.py
@@ -1,8 +1,7 @@
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAdminUser
-from rest_framework.viewsets import ModelViewSet, ViewSet
-from rest_framework.authtoken.models import Token
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
 from apps.api.v1.serializers import AppSerializer
@@ -33,7 +32,18 @@ class AppViewSet(ModelViewSet):
     """
 
     serializer_class = AppSerializer
-    queryset = App.objects.all()
     authentication_classes = (SessionAuthentication, TokenAuthentication)
-    permission_classes = [IsAdminUser, IsOwner]
+    permission_classes = [IsAdminUser | IsOwner]
     http_method_names = ["get", "post", "put", "patch", "delete"]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all apps
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        if user.is_anonymous:
+            raise PermissionDenied(
+                {"detail": "You do not have permission to perform this action."}
+            )
+        return App.objects.filter(user=user)
